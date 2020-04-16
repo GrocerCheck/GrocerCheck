9dd383badd645d65356a28f466a9a2753803a023
Brian Chen <ihasdapi@gmail.com>
2020-04-15 19:12:21 -0700

    minor edits

--- grocercheck/grocercheck/urls.py
+++ grocercheck/grocercheck/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+        path('admin/', admin.site.urls),
+        path('catalog/', include('catalog.urls')),
+        path('', RedirectView.as_view(url='catalog/')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
