6727f82829e7a8eec290b9999ff5a6051c080bc2
Brian Chen <ihasdapi@gmail.com>
2020-04-15 19:54:06 -0700

    minor edits

--- grocercheck/grocercheck/urls.py
+++ grocercheck/grocercheck/urls.py
@@ -13,18 +13,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
+
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+        path('admin/', admin.site.urls),
+
 ]
 from django.urls import include
 
 urlpatterns += [
-	path('map/', include('map.urls')),
+        path('map/', include('map.urls')),
+
 ]
 
 from django.views.generic import RedirectView
 
 urlpatterns += [
+        path('', RedirectView.as_view(url='map/')),
+
+]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
