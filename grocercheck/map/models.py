4255e05adbd1b18c8df60b46ab9a3d41270ed76c
Andy Liang <andy0liang@gmail.com>
2020-04-20 23:13:28 -0700

    middle of testing 04/20/2020 11:13pm

--- grocercheck/map/models.py
+++ grocercheck/map/models.py
@@ -5,11 +5,17 @@ from django.db import models
 
 
 class Store(models.Model):
-	''' MODEL REPRESENTING A GROCERY STORE '''
-	name = models.CharField(max_length=100)
-	busyness = models.IntegerField()
+    ''' MODEL REPRESENTING A GROCERY STORE '''
+    name = models.CharField(max_length=100)
+    busyness = models.IntegerField()
+    lat = models.CharField(max_length=10)
+    lon = models.CharField(max_length=10)
+    address = models.CharField(max_length=100)
+    hours = models.CharField(max_length=10)
+    placeID = models.CharField(max_length=100)
 
-	def __str__(self):
-		return self.name
+        
+    def __str__(self):
+        return self.name
 
 	
--- grocercheck/map/views.py
+++ grocercheck/map/views.py
@@ -5,6 +5,11 @@ from django.shortcuts import render
 
 
 from map.models import Store
+import json
 
 def index(request):
-	return render(request,'index.html')
+    context = {}
+    context["stores"] = json.dumps(['hi','test1','test2'])
+    context["lat"] = json.dumps(['49.2399','49.2499','49.2399'])
+    context['lon'] = json.dumps(['-123.1251','-123.1251','-123.1351'])
+    return render(request,'index.html',context=context)
