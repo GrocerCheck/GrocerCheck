84b8fdb6f357daa44a5b00d800c54dfbe1a0d47f
Brian Chen <ihasdapi@gmail.com>
2020-04-25 16:36:17 -0700

    apr 25 3:26pm

--- grocercheck/map/models.py
+++ grocercheck/map/models.py
@@ -7,12 +7,11 @@ from django.db import models
 class Store(models.Model):
     ''' MODEL REPRESENTING A GROCERY STORE '''
     name = models.CharField(max_length=100)
-    busyness = models.IntegerField()
     lat = models.CharField(max_length=10)
     lng = models.CharField(max_length=10)
     address = models.CharField(max_length=100)
     hours = models.CharField(max_length=10)
-    placeID = models.CharField(max_length=100)
+    place_id = models.CharField(max_length=100)
 
      
     def __str__(self):
