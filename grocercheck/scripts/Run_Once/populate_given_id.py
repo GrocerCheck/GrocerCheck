beee5455ffe90bd2287f34c4e66a52fbb10ea614
Brian Chen <ihasdapi@gmail.com>
2020-04-25 23:03:30 -0700

    worked out update row

--- grocercheck/scripts/Run_Once/populate_given_id.py
+++ grocercheck/scripts/Run_Once/populate_given_id.py
@@ -23,27 +23,36 @@ def get_columns(conn, col):
     col = cur.fetchall()
     return col
 
-
-def gen_days():
-    days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
-    out = ""
-    for d in days:
-        for h in range(24):
-            if h < 10:
-                out = out + ", set {day}0{hour} = {obrac}{day}{hour}{cbrac}".format(day=d, hour=h, obrac= "{", cbrac ="}")
-            else:
-                out = out + ", set {day}{hour} = {obrac}{day}{hour}{cbrac}".format(day=d, hour=h, obrac= "{", cbrac ="}")
-    return out
-
 def update_row(conn, data, row_id):
-    com1= "UPDATE map_store, SET name = {name}, SET lat = {lat}, SET lng = {lng}, SET address = {address}, SET live_busyness = {live_busyness}, WHERE id = {rowID}"
-    com1 = com1.format(live_busyness = data['current_popularity'], name =data['name'], lat = data['coordinates']['lat'], lng = data['coordinates']['lng'], address = data['address'], rowID = row_id)
-    print(com1)
-
+    days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+    print(data['name'])
     cur = conn.cursor()
-    cur.execute(com1)
+    cur.execute("UPDATE map_store SET name=? WHERE id=?", (data['name'], row_id))
+    cur.execute("UPDATE map_store SET lat=? WHERE id=?", (data['coordinates']['lat'], row_id))
+    cur.execute("UPDATE map_store SET lng=? WHERE id=?", (data['coordinates']['lng'], row_id))
+    cur.execute("UPDATE map_store SET address=? WHERE id=?", (data['address'], row_id))
+    cur.execute("UPDATE map_store SET live_busyness=? WHERE id=?", (data['current_popularity'], row_id))
+    for day in range(7):
+        for hour in range(24):
+            if hour < 10:
+                command = "UPDATE map_store SET {day}0{hour}=? WHERE id=?".format(day=days[day], hour=hour)
+                print(command)
+                cur.execute(command, (data['populartimes'][day]['data'][hour], row_id))
+            else:
+                cur.execute("UPDATE map_store SET {day}{hour}=? WHERE id=?".format(day=days[day], hour=hour), (data['populartimes'][day]['data'][hour], row_id))
+    conn.commit()
 
-conn = create_connection("/home/ihasdapie/Grocer_Check_Project/Org/GrocerCheck/grocercheck/db1.sqlite3")
+    print("Data updated successfully")
+
+
+
+
+
+
+
+
+
+conn = create_connection("/home/ihasdapie/Grocer_Check_Project/Org/GrocerCheck/grocercheck/test.sqlite3")
 
 
 API_KEY = open("/home/ihasdapie/keys/gmapkey.txt").readline()
