f26c0404ebab5c89b505e57f79dd6f8b413442c0
ihasdapie <ihasdapi@gmail.com>
2020-04-30 04:46:15 +0000

    changed to procedural set-null

--- grocercheck/map/current_scraper.py
+++ grocercheck/map/current_scraper.py
@@ -28,16 +28,17 @@ def update_row(conn, data, row_id):
     log = []
     days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
     cur = conn.cursor()
-
     try:
         if (data['current_popularity'] is None) == False:
             cur.execute("UPDATE map_store SET live_busyness=? WHERE id=?", (data['current_popularity'], row_id))
 
         else:
+            cur.execute("UPDATE map_store SET live_busyness=NULL WHERE id=?", (row_id)) #if no live busyness, set to null (clean up!)
             log.append("CANNOT RETRIEVE LIVE BUSYNESS FOR STORE id"+str(row_id))
     except:
         log.append("CURRENT POPULARITY KEY ERROR FOR STORE id"+str(row_id))
     conn.commit()
+    print(data['name'], row_id, "complete")
     return(log)
 
 def get_valid_id(conn):
@@ -73,17 +74,11 @@ def update_current_popularity(formatted_address_list, doBackup, doLog, conn):
                 LOG.write(entry)
                 LOG.write("\r\n")
 
-def set_live_null(conn):
-    cur = conn.cursor()
-    cur.execute("UPDATE  map_store SET live_busyness = NULL") #set all to NULL before running script
-    conn.commit()
-
 
 def run_scraper(country, doBackup, doLog):
     global LOG
     conn = create_connection("/home/bitnami/apps/django/django_projects/GrocerCheck/grocercheck/db1.sqlite3")
     try:
-        set_live_null(conn)
         update_current_popularity(get_formatted_addresses(country, conn), doBackup, doLog, conn)
 
     except:
