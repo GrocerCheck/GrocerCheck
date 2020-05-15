ea52951ece8988572e1b84861d845e0635f7a58e
Brian Chen <ihasdapi@gmail.com>
2020-05-15 03:30:25 -0700

    populate db, add cards to covidwatch

--- grocercheck/map/admin.py
+++ grocercheck/map/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Store
+from .models import blog_entry
 # Register your models here.
 
 admin.site.register(Store)
-
+admin.site.register(blog_entry)
