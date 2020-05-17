89c8d222b59f1e6ef00509bf66a2693c0af39ece
Brian Chen <ihasdapi@gmail.com>
2020-05-17 12:48:48 -0700

    fix gencoords, list coords for greater seattle area

--- grocercheck/scripts/populatecity/gencoords.py
+++ grocercheck/scripts/populatecity/gencoords.py
@@ -15,42 +15,49 @@ def haversine(lat1, lon1, lat2, lon2):
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
     c = 2 * asin(sqrt(a))
     r = 6372.8
-    return c * r * 1000
-
+    return c * r 
 
 def gen_coords(border, spacing):
     """
     :param border: list of list of coordinates in tuples outlining a rectangular area for which coords are to be generated.
-    :param hotspots: list of tuples for areas in which more coordinates are to be generated
-    :param spacing: normal distance, in meters, between generated coords
-    :param hotspot_spacing: distance, in meters, for coord spacing between hotspots
-
-    note: each border rectange must be given in form [(top,left), (top,right), (bottom, right), (bottom,left)]
-    Order matters -- try to only go clockwise or only counterclockwise! Refer to matplotlib path.
+    :param spacing: normal distance, in kilometers, between generated coords
+    
+    note: each border rectange must be given in form [(bottom,left), (top, right)]
+    
     output: list of tuples, as per example border input
-
-    coords within 2*np.ln(spacing)*np.sqrt(spacing) of each hotspot will be clustered closer together as per hotspot_spacing
     """
+    
     out_coords = []
     for bound in border:
-        cx=bound[3][0]
-        cy=bound[3][1]
-        diffx = haversine(bound[0][0], bound[0][1], bound[1][0], bound[1][1])
-        diffy = haversine(bound[0][0], bound[0][1], bound[3][0], bound[3][1])
+        bottomleft = bound[0]
+        topright = bound[1]
+        bottomright = (bound[1][0], bound[0][1])
+        topleft = (bound[0][0], bound[1][1])
+                
+        
+        cx=bottomleft[0]
+        cy=bottomleft[1]
+        
+        diffx = haversine(bottomright[0], bottomright[1], bottomleft[0], bottomleft[1])
+        diffy = haversine(topleft[0], topleft[1], bottomleft[0], bottomleft[1])
 
         numx = int(diffx//spacing)
         numy = int(diffy//spacing)
-        deltax = abs(bound[1][0] - bound[0][0])/numx
-        deltay = abs(bound[0][1] - bound[3][1])/numy
+        
+        deltax = abs((bottomright[0]-bottomleft[0]))/numx
+        deltay = abs((topleft[1]-bottomleft[1]))/numy
+        #~ deltax = 1
+        #~ deltay = 1
 
-        out_coords.append((cx, cy))
         for x in range(numx):
-            cx = cx + deltax
             for y in range(numy):
-                cy = cy + deltay
                 out_coords.append((cx, cy))
+                cy = cy + deltay
+            cy = bottomleft[1]
+            cx = cx + deltax
+            
+        #~ print(" numx ", numx, " numy ", numy, "deltax", deltax, "deltay", deltay, "diffx", diffx, "diffy", diffy)
     return out_coords
-
 
 
 
--- grocercheck/scripts/populatecity/scrape_city.py
+++ grocercheck/scripts/populatecity/scrape_city.py
@@ -2,21 +2,78 @@ import get_places
 import gencoords
 import add_place_detail
 import os
+import matplotlib.pyplot as plt
+import numpy as np
+#Bounds must be given as a list of list of tuples (lat, lng) in the format
+    #[(bottom,left), (top, right)
 
 bounds = [
+#SEATTLE
+        [
+        (47.48290687, -122.39343805),
+        (47.59601325, -122.27670831), #leschi
+            ],
+        [
+        (47.59417591, -122.34250155), #chinatown to madison park corner
+        (47.64242772, -122.27787883),
+            ],
+        [
+        (47.59489101, -122.35767016),
+        (47.64880977, -122.31767306), #downtown, no queen anne
+        ],
+        
+        [
+        (47.62118399, -122.37822272),
+        (47.65334064, -122.34560706), #queen anne
+        ],
+        
+        [
+        (47.62956846, -122.41305438),
+        (47.66089822, -122.38710229),
+        ], # magnolia
+        
+        [
+        (47.64785419, -122.39418722), #university & above, overlaps magnolia a bit
+        (47.73281416, -122.27502302),
+        ],
+        [
+        (47.52982143, -122.24156467),
+        (47.59538492, -122.21409885), #mercer island
+        ],
+        [
+        (47.57883392, -122.19693271), #bellevue to lower redmond
+        (47.6626065, -122.10766879),
         ]
+        [
+        (47.61515644, -122.23785054), #clyde hill
+        (47.64617081, -122.19317176),
+        ]
+        [
+        (47.6555421, -122.20810629),
+        (47.77150651, -122.07764364), #redmond to bothell
+        ]
+        [
+        (47.58009247, -122.06147627), #sammamish
+        (47.63771709, -122.0016913), 
+        ]
+        
+]
+
+#API_KEY = open("/home/ihasdapie/keys/gmapkey.txt").readline()
+
+#DATABASE_DIR = os.path.dirname(os.path.dirname(os.getcwd())) + "db1.sqlite3"
+
+coord_list = gencoords.gen_coords(bounds, 1)
+print(len(coord_list))
+x, y= np.array(coord_list).T
+
+plt.scatter(x,y)
+plt.show()
 
 
 
-API_KEY = open("/home/ihasdapie/keys/gmapkey.txt").readline()
-
-
-DATABASE_DIR = os.path.dirname(os.path.dirname(os.getcwd())) + "db1.sqlite3"
-
-coord_list = gencoords.gen_coords(bounds)
-first_id = get_places.getplaces(API_KEY, coord_list, DATABASE_DIR)
-add_place_detail(API_KEY, first_id, DATABASE_DIR)
-
+#first_id = get_places.getplaces(API_KEY, coord_list, DATABASE_DIR)
+#add_place_detail(API_KEY, first_id, DATABASE_DIR)
 
 
 
