65d5ffc78caa33a2db931d5b845c75de0286eb5a
Andy Liang <andy0liang@gmail.com>
2020-05-17 23:51:03 -0700

    fixed url with cities and other pages

--- grocercheck/map/urls.py
+++ grocercheck/map/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('<str:city>/', views.index),
-	path('', views.index, name='index'),
+        
+	
         path('about/', views.about, name='about'),
         path('covidwatch/<int:articleid>/', views.article),
         path('covidwatch/', views.covidwatch, name='covidwatch'),
         path('sponsors/', views.sponsors, name='sponsors'),
         path('contact/', views.contact, name='contact'),
         path('media/', views.media, name='media'),
+        path('<str:city>/', views.index),
+        path('', views.index, name='index'),
                 ]
