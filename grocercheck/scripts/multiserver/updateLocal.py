e56d2356daecddb008316e05b35c77900166e0ae
Brian Chen <ihasdapi@gmail.com>
2020-05-21 22:42:22 -0700

    add test db

--- grocercheck/scripts/multiserver/updateLocal.py
+++ grocercheck/scripts/multiserver/updateLocal.py
@@ -1,2 +1,8 @@
 import psycog2
 import sqlite3
+
+def create_pgsql_connection(dbname, user, password, host, port):
+    conn = psycog2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+
+
+
