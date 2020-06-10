fa8ec26a0dff736116fbb8a6982228e1a91c222a
Andy Liang <andy0liang@gmail.com>
2020-06-10 14:46:52 -0700

    updated open and close processing in views

--- grocercheck/map/views.py
+++ grocercheck/map/views.py
@@ -102,54 +102,48 @@ def index(request, city="nocity"):
             if(hourstring==None):
                 context['openn'].append(0)
             else:
-                hours = hourstring.split(": ")[1]
+                hours = hourstring.split(": ")[1]#Friday: 8:00 AM - 7:00 PM
+                #8:00 AM - 7:00 PM
                 if (' 24' in hours):
                     context['openn'].append(1)
                 elif ('–' not in hours):
                     context['openn'].append(0)
                 else:
-                    hours = hours.split(" – ")
-                    oh, om = int(hours[0].split(':')[0]), int(hours[0].split(':')[1][:2]) #opening hour, opening minute
-                    ch, cm = int(hours[1].split(':')[0]), int(hours[1].split(':')[1][:2]) #closing hour, closing minute
+                    hours = hours.split(" – ")#[8:00 AM,7:00 PM] 
+                    ostring = hours[0].split(':')#[8,00 AM]
+                    cstring = hours[1].split(':')#[7,00 PM]
 
-                    if ((hours[0][-2:] == "PM") and (oh != 12)):
-                        oh += 12
-                    if ((hours[1][-2:] == "PM") and (ch !=12)): #12PM is noon, 12AM is midnight
-                        ch += 12
-                    if ((hours[0][-2:] == "AM") and (oh == 12)):
-                        oh = 0 #if it opens at midnight, set to 0 for comparison
-                    if ((hours[1][-2:] == "AM") and (ch == 12)):
-                        ch = 24
-
-                    if ((hours[1][-2:] == "AM") and (ch < oh)) or ((hours[1][-2:] == "AM") and (ch == 24)): #check if the store closes after midnight or at midnight
-                        if (ch == 24):
-                            if ((localhour < ch) and (localhour > oh)):
-                                context['openn'].append(1)
-                            else:
-                                context['openn'].append(0)
-                        else:
-                            if (localhour >= oh):
-                                context['openn'].append(1)
-                            if (localhour < ch):
-                                context['openn'].append(1)
-
-                            elif (localhour == ch and localminute < cm):
-                                context['openn'].append(1)
+                    oh, om, omod = int(ostring[0]), int(ostring[1][:2]), ostring[1][-2:] #opening hour, opening minute, opening modifier
+                    ch, cm, cmod = int(cstring[0]), int(cstring[1][:2]), cstring[1][-2:] #closing hour, closing minute, closing modifier
+                    
+                    if omod=='AM' and oh==12:
+                        oh = 0
+                    if omod=='PM' and oh!=12:
+                        oh+=12
+                    if cmod=='PM' and ch!=12:
+                        ch+=12
+                    if cmod=='AM' and ch==12:
+                        ch==24
+                    if cmod=='AM' and omod=='AM' and ch<oh and ch!=12:
+                        ch+=24
+                    if cmod=='AM' and omod=='PM':
+                        ch+=24
+                    if localhour>oh and localhour<ch:
+                        context['openn'].append(1)
+                    elif localhour==oh and localminute>=om:
+                        context['openn'].append(1)
+                    elif localhour==ch and localminte<ch:
+                        context['openn'].append(1)
                     else:
-                            if(localhour>oh and localhour<ch):
-                                context['openn'].append(1)
-                            elif(localhour==oh and localminute>=om):
-                                context['openn'].append(1)
-                            elif(localhour==ch and localminute<cm):
-                                context['openn'].append(1)
-                            else:
-                                context['openn'].append(0)
+                        context['openn'].append(0)
 
+                    
 
     finalcontext = {}
 
     for key in context.keys():
         finalcontext[key] = json.dumps(context[key])
+        
 
     finalcontext['size'] = json.dumps([len(context['busyness'])])
 
